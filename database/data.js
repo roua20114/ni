7d62cf35a3217e986ba095e95255bce095fd9c69
unknown <rouazarrad2017@gmail.com>
2022-12-02 02:35:29 +0100

    first commit

---
+++ database/data.js
@@ -0,0 +1,96 @@
+export default [
+    {
+        id: 1,
+        question : "Pourquoi faut-il pincer le réservoir du préservatif au moment où on le met ?",
+        options : [
+            'Pour en chasse air',
+            'Pour donner plus de plaisir',
+    
+        ]
+    },
+    {
+        id: 2,
+        question : "Le VIH est un virus qui s’attaque au…",
+        options : [
+            'Systéme immunitaire',
+            'Systéme reproductif',
+            'Systéme digestif'
+          
+        ]
+    },
+    {
+        id: 3,
+        question : "Une personne séropositive qui a une charge virale indétectable… ::",
+        options : [
+            "N'a, grace aux traitementss, presque plus de VIH dans son organisme ",
+            "N'a plus du tout de VIH dans son corps",
+            'A du VIH dans le cors mais on ne sait pas dire si elle en a peu ou beaucoup',
+        ]
+    },
+    {
+        id: 4,
+        question : "Le préservatif protège contre toutes les infections sexuellement transmissibles ?",
+        options : [
+            'Non',
+            'Oui',
+            
+        ]
+    },
+    {
+        id: 5,
+        question : "Être infecté·e par une IST augmente le risque d’être infecté·e par le VIH ?",
+        options : [
+            'Vrai',
+            'Faux, je ne peux etre infecté que par une IST à ala fois',
+        "Faux, etre infecté par une IST augmente pas la risque d'etre infecté par le VIH  ",
+        ]
+    },
+    {
+        id: 6,
+        question : "Une personne séronégative peut prendre des médicaments pour ne pas être infectée par le VIH ?",
+        options : [
+            'Vrai',
+            'Faux',
+        "vrai mais uniquemnet pour les femmes  ",
+        ]
+    }, 
+   
+    {
+        id: 7,
+        question : " La fellation sans préservatif représente-t-elle un risque de transmission du VIH ?",
+        options : [
+            'Vrai',
+            'Faux',
+      
+        ]
+    }, 
+    {
+        id: 8,
+        question : " Est-ce que je peux être contaminé·e par le VIH avec une piqûre de moustique ?",
+        options : [
+            'Non, le VIH ne se transmet pas par les piqures de moustique',
+            "oui si c'est un moustique tigre",
+      
+        ]
+    }, 
+    {
+        id: 9,
+        question : " Je peux avoir un rapport sexuel sans préservatif avec une personne vivant avec le VIH sans risque d’être infecté·e ?",
+        options : [
+            'Oui mais uniquement si sa charge virale ent indétectable',
+            'Oui mais seulement si elle a pas eu de relations sexuelles depuis 6 mois',
+      
+        ]
+    }, 
+    {
+        id: 10,
+        question : " Je peux utiliser n’importe quelle sorte de lubrifiant avec un préservatif ?",
+        options : [
+            "Non,seuls les lubrifiants à base d'eau ou de silicone",
+            'Oui, tous les lubrifiants peuvent etre utilisés',
+      
+        ]
+    }, 
+];
+
+export const answers = [0, 0, 0, 0, 0, 0, 0, 0, 0,0];
